48a6f8aa56b1c7923c0d18123e45e98688edbc4e
Bianwy <bianwy025@qq.com>
2020-03-05 01:43:10 +0800

    实现主要的数组的输出和结构体的输出。

--- pch.cpp
+++ pch.cpp
@@ -14,6 +14,7 @@ typedef int(*func2ptr) (long long lLoginID,
 	int nChannelID,
 	unsigned long dwAlarmType, // DWORD
 	bool bNeedPicFile,
+	fAnalyzerDataCallBack cbAnalyzerData,
 	unsigned int dwUser,
 	void* Reserved);
 
@@ -25,9 +26,87 @@ typedef int(*func2ptr) (long long lLoginID,
 //#pragma comment(lib, "lib/win32/mydll.lib")	
 #endif
 
+typedef struct tagDEV_EVENT_ANATOMY_TEMP_DETECT_INFO_1
+{
+	int                         nChannelID;				// 通道号
+	int                         nAction;				// 1:开始 2:停止
+	char                        szName[128];			// 事件名称
+	double                      PTS;					// 时间戳(单位是毫秒)
+	//NET_TIME_EX                 UTC;					// 事件发生的时间
+	int                         nEventID;				// 事件ID
+
+	//EM_CLASS_TYPE               emClassType;			// 智能事件所属大类
+	UINT						nPresetID;				// 事件触发的预置点号, 从1开始, 没有该字段,表示预置点未知
+	//NET_MAN_TEMP_INFO			stManTempInfo;          // 区域内人员体温信息 
+	//NET_VIS_SCENE_IMAGE			stVisSceneImage;		// 可见光全景图
+	//NET_THERMAL_SCENE_IMAGE		stThermalSceneImage;	// 热成像全景图
+	UINT						nSequence;				// 帧序号
+	UINT						nEventRelevanceID;		// 事件关联ID
+	BYTE						byReserved[1020];		// 预留字段
+} DEV_EVENT_ANATOMY_TEMP_DETECT_INFO_1;
+
+static DEV_EVENT_ANATOMY_TEMP_DETECT_INFO_1* gPAlarmInfo1;
 static DEV_EVENT_ANATOMY_TEMP_DETECT_INFO* gPAlarmInfo;
 static BYTE *gPBuffer;
 static DWORD gDwBufSize;
+
+int  CALLBACK AnalyzerDataCallBack(LLONG lAnalyzerHandle, DWORD dwAlarmType, void* pAlarmInfo, BYTE *pBuffer, DWORD dwBufSize, LDWORD dwUser, int nSequence, void *reserved)
+{
+	// pBuffer 中是图片信息，保存下来供返回
+	gPBuffer = pBuffer;
+	gDwBufSize = dwBufSize;
+	if (EVENT_IVS_ANATOMY_TEMP_DETECT == dwAlarmType)
+	{
+		DEV_EVENT_ANATOMY_TEMP_DETECT_INFO* pInfo = (DEV_EVENT_ANATOMY_TEMP_DETECT_INFO*)pAlarmInfo;
+		printf("AnalyzerDataCallBack:anatomy temp detect,nChannelID:%d, nAction:%d, szName:%s,"
+			"nEventID:%d, nSequence:%d, nPresetID:%d, PTS:%d\n",
+			pInfo->nChannelID,
+			pInfo->nAction,
+			pInfo->szName,
+			pInfo->nEventID,
+			pInfo->nSequence,
+			pInfo->nPresetID,
+			pInfo->PTS
+		);
+		// 保存结构体信息
+		gPAlarmInfo1->nChannelID = pInfo->nChannelID;
+		gPAlarmInfo1->nAction = pInfo->nAction;
+		strncpy_s(gPAlarmInfo1->szName, pInfo->szName, sizeof(pInfo->szName));
+		gPAlarmInfo1->nEventID = pInfo->nEventID;
+		gPAlarmInfo1->nSequence = pInfo->nSequence;
+		gPAlarmInfo1->nPresetID = pInfo->nPresetID;
+		gPAlarmInfo1->PTS = pInfo->PTS;
+
+
+		// 区域内人员体温信息 
+		// 人脸小图特征值起始位置：pBuffer + stManTempInfo.nOffset
+		// 人脸小图特征值长度：stManTempInfo.nLength
+		const NET_MAN_TEMP_INFO	&stManTempInfo = pInfo->stManTempInfo;
+		printf("区域内人员体温信息:nObjectID:%d, dbHighTemp:%d, nTempUnit:%d,bIsOverTemp:%d, bIsUnderTemp:%d\n",
+			stManTempInfo.nObjectID, stManTempInfo.dbHighTemp,
+			stManTempInfo.nTempUnit, stManTempInfo.bIsOverTemp,
+			stManTempInfo.bIsUnderTemp);
+
+		// 可见光全景图		
+		const NET_VIS_SCENE_IMAGE	&stVisSceneImage = pInfo->stVisSceneImage;
+		printf("可见光全景图信息：nOffset:%d, nLength:%d, nWidth:%d,nHeight:%d",
+			stVisSceneImage.nOffset, stVisSceneImage.nLength, stVisSceneImage.nWidth, stVisSceneImage.nHeight);
+		// 可见光全景图起始位置：pBuffer + stVisSceneImage.nOffset
+		// 可见光全景图长度：stVisSceneImage.nLength
+
+		// 热成像全景图		
+		const NET_THERMAL_SCENE_IMAGE	&stThermalSceneImage = pInfo->stThermalSceneImage;
+		printf("热成像全景图信息：nOffset:%d, nLength:%d, nWidth:%d,nHeight:%d",
+			stThermalSceneImage.nOffset, stThermalSceneImage.nLength, stThermalSceneImage.nWidth, stThermalSceneImage.nHeight);
+
+		// 热成像全景图起始位置：pBuffer + stThermalSceneImage.nOffset
+		// 热成像全景图长度：stThermalSceneImage.nLength
+
+	}
+
+	return 0;
+}
+
 
 int Add(int a, int b) {
 	int result = a + b;
@@ -125,7 +204,7 @@ int CALL_METHOD CICDI_CLIENT_FetchPicture_DetectTemp(
 	void* Reserved,
 	// 下面为出参
 	//unsigned long *dwAlarmType,
-	void *pAlarmInfo,
+	DEV_EVENT_ANATOMY_TEMP_DETECT_INFO_1 *pAlarmInfo,
 	unsigned char *pBuffer,
 	unsigned int *dwBuffSize) {
 	cout << "in c++, CICDI_CLIENT_FetchPicture_DetectTemp, params:" << endl;
@@ -150,11 +229,17 @@ int CALL_METHOD CICDI_CLIENT_FetchPicture_DetectTemp(
 		cout << "gagaga2" << endl;
 		if (func != NULL)
 		{
-			int lRealloadHandle = (*func)(lLoginID, nChannelID, dwAlarmType, bNeedPicFile, dwUser, Reserved);
+			int lRealloadHandle = (*func)(lLoginID, nChannelID, dwAlarmType, bNeedPicFile, AnalyzerDataCallBack, dwUser, Reserved);
 			cout << "lRealloadHandle=" << lRealloadHandle << endl;
 			if (lRealloadHandle)
 			{
 				cout<<"CLIENT_RealLoadPictureEx success\n";
+				*dwBuffSize = gDwBufSize;
+				for (int i = 0; i < gDwBufSize; i++) {
+					pBuffer[i] = gPBuffer[i];
+				}
+				// 结构体赋值
+				*pAlarmInfo = *gPAlarmInfo1;
 			}
 			else
 			{

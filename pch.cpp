9d002e84c66044b6c38882a403c6d4ec6e7403c2
Bianwy <bianwy025@qq.com>
2020-03-04 16:56:03 +0800

    增加初始化项目架构和最基本的示例。

---
+++ pch.cpp
@@ -0,0 +1,13 @@
+﻿// pch.cpp: 与预编译标头对应的源文件
+
+#include "pch.h"
+
+// 当使用预编译的头时，需要使用此源文件，编译才能成功。
+#include <iostream>
+using namespace std;
+
+int Add(int a, int b) {
+	int result = a + b;
+	cout << "In dll..., Add function, a=" << a << ",b=" << b << ", result = " << result << endl;
+	return result;
+}
